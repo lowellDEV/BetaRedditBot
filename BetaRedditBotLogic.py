a2c8d24a9c2789bb76dd2db4f1e39a25c6174b71
LowellDev <lbdev@outlook.com>
2017-06-07 21:06:46 -0500

    added code

---
+++ BetaRedditBotLogic.py
@@ -0,0 +1,24 @@
+import puniX
+import praw
+reddit =None
+
+def TestAuth():
+    if reddit is None:
+        Authenticate()
+    user = reddit.user
+    if user is not None:
+        print(user.me())
+        count =0
+        for item in reddit.inbox.unread(limit=None):
+            if isInstance(item, Comment):
+                count= count+1
+        print('Unread: '+ str(count))
+        
+      
+def Authenticate():
+    global reddit
+    reddit = praw.Reddit(client_id=puniX.botID,
+                     client_secret=puniX.botSecret,
+                     password=puniX.password,
+                     user_agent='pythonScript/BetaRedditBot by /u/'+puniX.username,
+                     username=puniX.username)
