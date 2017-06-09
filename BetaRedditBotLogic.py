2b99efe184193432c103552d5840b5d157f1a4d7
LowellDev <lbdev@outlook.com>
2017-06-08 19:12:51 -0500

    added some comments

--- BetaRedditBotLogic.py
+++ BetaRedditBotLogic.py
@@ -5,6 +5,7 @@ import sys
 """Contains the logic for the bot"""
 count =0
 def main():
+    """Run the Logic"""
     bot.Authenticate()
     if bot.reddit is None:
         print("Authentication Failed, check Credentials File")
@@ -16,13 +17,15 @@ def main():
 
     
 def search():
+"""Search for users with pm in the username"""
     while True:
         pm = None
         pm = bot.searchForPM()
         if pm is not None:
             congratulate(pm)
 
-def message():
+def congratulate(pm):
+    """Send Scripted Message to user"""
     msg= "Hello,\n This is a scripted message to greet people like you! I am curious to know if you have gotten your requested pms. Message back and you will get a human responder (probably).\n\nThanks!"
     bot.message(pm,"Congrats!",msg)
     count+=1
